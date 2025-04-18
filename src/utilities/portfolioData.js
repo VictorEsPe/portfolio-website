30826eeb3f9cc8ffe634d64afb66d51b73c52042
Victor Estevam Pereira <victorestevamp@gmail.com>
2025-04-18 15:47:02 -0300

    projeto pokedex adicionado ao portfolio + melhora na nomenclatura das variáveis de imagem do "portfolioData"

--- src/utilities/portfolioData.js
+++ src/utilities/portfolioData.js
@@ -1,44 +1,52 @@
-import IMG1 from '../assets/rdr-portfolio.png'
-import IMG2 from '../assets/formulario-login.png'
-import IMG3 from '../assets/calculadora-idade.png'
-import IMG4 from '../assets/Advice-generator.png'
-import IMG5 from '../assets/audiobook-player.png'
+import rdrCarousel from '../assets/rdr-portfolio.png'
+import loginForm from '../assets/formulario-login.png'
+import ageCalculator from '../assets/calculadora-idade.png'
+import adviceGenerator from '../assets/Advice-generator.png'
+import audiobook from '../assets/audiobook-player.png'
+import pokedex from '../assets/pokedex.png'
 
 const portfolioData = [
   {
     id: 1,
-    image: IMG1,
+    image: rdrCarousel,
     title: 'Carrossel Red Dead Redemption 2',
     github: 'https://github.com/VictorEsPe/Carrossel-Red-Dead-Redemption',
     demo: 'https://victorespe.github.io/Carrossel-Red-Dead-Redemption/',
   },
   {
     id: 2,
-    image: IMG2,
+    image: loginForm,
     title: 'Formulário de login',
     github: 'https://github.com/VictorEsPe/formulario-de-login',
     demo: 'https://victorespe.github.io/formulario-de-login/',
   },
   {
     id: 3,
-    image: IMG3,
+    image: ageCalculator,
     title: 'Calculadora de idade',
     github: 'https://github.com/VictorEsPe/calculadora-de-idade',
     demo: 'https://victorespe.github.io/calculadora-de-idade/',
   },
   {
     id: 4,
-    image: IMG4,
+    image: adviceGenerator,
     title: 'Gerador de conselhos aleatórios',
     github: 'https://github.com/VictorEsPe/Advice-generator-API',
     demo: 'https://victorespe.github.io/Advice-generator-API/',
   },
   {
     id: 5,
-    image: IMG5,
+    image: audiobook,
     title: 'Audiobook player',
     github: 'https://github.com/VictorEsPe/Audiobook-player',
     demo: 'https://victorespe.github.io/Audiobook-player/',
+  },
+  {
+    id: 6,
+    image: pokedex,
+    title: 'Pokédex',
+    github: 'https://github.com/VictorEsPe/Pokedex',
+    demo: 'https://my-react-pokedex-app.vercel.app/',
   },
 ]
 
